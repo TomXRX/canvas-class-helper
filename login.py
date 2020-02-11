1a163248868a0b1e60b7267243df6c4c97642597
TomXRX <49830204+TomXRX@users.noreply.github.com>
2020-02-11 22:16:46 +0800

    Update login.py

--- login.py
+++ login.py
@@ -16,8 +16,21 @@ assert 密码!="password","请修改密码"
 
 from selener.base import *
 import time
-track=["English"]
+track=["English","Conferences",]
+track2=["Join","Microphone"]
 with LoginSearcher(r"https://canvas.instructure.com/login/canvas",login=0) as a:
     a.login(邮箱,密码,"电子邮件")
     a.wait_click(track)
+    print("all done")
+    print("waiting...")
+    while 1:
+        if a.wait_click(track,check=2):break
+
+
+    print("all done")
+    print("DO NOT CLOSE THIS TERMINAL")
+    while 0:
+        try:
+            input()
+        except:pass
     input()

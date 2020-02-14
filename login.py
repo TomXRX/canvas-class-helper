f43fe429fc2bf57e136e083e770508e8200281a1
TomXRX <49830204+TomXRX@users.noreply.github.com>
2020-02-14 08:58:40 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,7 @@
-邮箱="mail@emal.com"
+邮箱="mail@email.com"
 密码="password"
+课程="class"
+#写部分名称用于匹配即可
 
 
 
@@ -9,24 +11,46 @@
 
 
 
-
-
-assert 邮箱!="mail@emal.com", "请修改邮箱"
+assert 邮箱!="mail@email.com", "请修改邮箱"
 assert 密码!="password","请修改密码"
+assert 课程!="class","请修改课程"
 
 from selener.base import *
-import time
-track=["English","Conferences",]
-track2=["Join","Microphone"]
-with LoginSearcher(r"https://canvas.instructure.com/login/canvas",login=0) as a:
+
+Time="8:45"
+#使用电脑的系统时间
+import time,datetime
+def alarm(Time):
+                cycle=24*60*60
+                assert len(Time) >= 4
+                if len(Time) <= 5: Time = Time + time.strftime(":%S")
+                if len(Time) < 10: Time = time.strftime('%Y-%m-%d ') + Time
+                Time = time.mktime(time.strptime(Time, '%Y-%m-%d %H:%M:%S'))
+                Time = Time - time.mktime(datetime.datetime.now().timetuple())
+                if Time <= 0: Time += cycle
+                return Time
+if 0:
+    if alarm(Time) - 60 * 10 > 0:
+
+        time.sleep(alarm(Time) - 60 * 10)
+
+        print("start in 10 min")
+        time.sleep(60 * 10)
+    else:
+        time.sleep(alarm(Time) - 60 * 10)
+
+print("start")
+
+track=[课程,"Conferences",]
+track2=["Join","仅聆听"]
+with LoginSearcher(r"https://canvas.instructure.com/login/canvas",login=0,wait=100) as a:
     a.login(邮箱,密码,"电子邮件")
     a.wait_click(track)
     print("all done")
-    print("waiting...") 
+    print("waiting...",end="")
     while 1:
         if a.wait_click(track2,check=2,timeout=30):break
         a.driver.refresh()
-
 
     print("all done")
     print("DO NOT CLOSE THIS TERMINAL")
